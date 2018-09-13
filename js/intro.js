f63a92e572cdb2ba07d3ca00343c5280f23521b1
Kevin McAlear <kevin.mcalear@gmail.com>
2018-09-13 13:41:37 +0200

    adds accent lights

--- js/intro.js
+++ js/intro.js
@@ -39,45 +39,47 @@ conversation += "*the parts of him he doesn't understand.*"
 let changeToEmotion = (emotion, duration=1, delay=0) => {
 	const feelings = {
 		sad: {
-			color:0x0000ff,
-			groundColor:0x02020b,
-			accentLight1:0x0000ff,
-			accentLight2:0x0000ff,
+			color: 0x0000ff,
+			groundColor: 0x02020b,
+			accentLight1: 0x0000ff,
+			accentLight2: 0x0000ff,
 		},
 		angry: {
-			color:0xff0000,
-			groundColor:0xff0000,
-			accentLight1:0xff0000,
-			accentLight2:0xff0000,
+			color: 0xff0000,
+			groundColor: 0xff0000,
+			accentLight1: 0xff0000,
+			accentLight2: 0xff0000,
 		},
 		sick: {
-			color:0x00ff00,
-			groundColor:0x00ff00,
-			accentLight1:0x00ff00,
-			accentLight2:0x00ff00,
+			color: 0x00ff00,
+			groundColor: 0x00ff00,
+			accentLight1: 0x00ff00,
+			accentLight2: 0x00ff00,
 		},
 		scared: {
-			color:0xa9a9a9,
-			groundColor:0xa9a9a9,
-			accentLight1:0xa9a9a9,
-			accentLight2:0xa9a9a9,
+			color: 0xa9a9a9,
+			groundColor: 0xa9a9a9,
+			accentLight1: 0xa9a9a9,
+			accentLight2: 0xa9a9a9,
 		},
 		happy: {
 			color: 0xffcc00,
 			groundColor: 0xffff00,
-			accentLight1:0xffcc00,
-			accentLight2:0xffcc00,
+			accentLight1: 0xffcc00,
+			accentLight2: 0xffcc00,
 		},
 		excited: {
 			color: 0xff8c00,
 			groundColor: 0xff8c00,
-			accentLight1:0xff8c00,
-			accentLight2:0xff8c00,
+			accentLight1: 0xff8c00,
+			accentLight2: 0xff8c00,
 		}
 	}
-	const feeling = feelings[emotion] === undefined ? { color:0xffffff, groundColor:0xffffff } : feelings[emotion];
+	const feeling = feelings[emotion] === undefined ? { color: 0xffffff, groundColor: 0xffffff } : feelings[emotion];
 	const color = new THREE.Color( feeling.color );
 	const groundColor = new THREE.Color( feeling.groundColor );
+	const accentLight1 = new THREE.Color( feeling.accentLight1 );
+	const accentLight2 = new THREE.Color( feeling.accentLight2 );
 	setTimeout(()=> {
 		TweenMax.to(hemiLight.color, duration, {
 		  r: color.r,
