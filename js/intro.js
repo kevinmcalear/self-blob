83c8e0485eee231e5933eb81878db71d1fe3b2df
Kevin McAlear <kevin.mcalear@gmail.com>
2018-09-13 02:29:06 +0200

    starting to build a conversation

--- js/intro.js
+++ js/intro.js
@@ -1,2 +1,42 @@
-var typing=Typing("Hello, how are you?");
+let conversation = "";
+conversation += "Hello.";
+conversation += "~1500~";
+conversation += "*.*";
+conversation += ", I'm Kevin.";
+conversation += "~1500~";
+conversation += "*Hello, I'm Kevin.*";
+conversation += "Well, I'm not actually Kevin.";
+conversation += "~800~";
+conversation += "*Well, I'm not actually Kevin.*";
+conversation += "I'm something else.";
+conversation += "~1500~";
+conversation += "*I'm something else.*";
+conversation += "I'm Kevin's hopes.";
+conversation += "~1500~";
+conversation += "*Kevin's hopes.*";
+conversation += "his dreams.";
+conversation += "~1500~";
+conversation += "*dreams.*";
+conversation += "past.";
+conversation += "~1500~";
+conversation += "*.*";
+conversation += " and present.";
+conversation += "~1500~";
+conversation += "*his past and present.*";
+conversation += "the jobs he's held.";
+conversation += "~1500~";
+conversation += "*the jobs he's held.*";
+conversation += "his fears.";
+conversation += "~1500~";
+conversation += "*.*";
+conversation += " and insic~500~*ic*ecuriet~500~*iet*ities.~300~";
+conversation += "*his fears and insecurities.*";
+conversation += "the parts of him he doesn't understand."
+conversation += "~1500~";
+conversation += "*the parts of him he doesn't understand.*"
+
+
+
+
+var typing=Typing(conversation, 90, 300, 50,'conversation-text');
 typing();
